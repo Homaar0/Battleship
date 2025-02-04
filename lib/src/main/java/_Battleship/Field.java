604ea5dae6f24cc136029c74bcc3431e0e23dd56
Homaar0 <144428272+Homaar0@users.noreply.github.com>
2025-02-04 10:40:24 +0100

    Printing field and getting ship coordinates

---
+++ lib/src/main/java/_Battleship/Field.java
@@ -0,0 +1,13 @@
+package _Battleship;
+
+public class Field {
+	
+	
+	void print() {
+		System.out.println("  1 2 3 4 5 6 7 8 9 10");
+		for (char i = 'A'; i <= 'J'; i++) {
+			System.out.println(i + " ~ ~ ~ ~ ~ ~ ~ ~ ~ ~");
+		}
+	}
+
+}
--- lib/src/main/java/_Battleship/Main.java
+++ lib/src/main/java/_Battleship/Main.java
@@ -1,10 +1,34 @@
 package _Battleship;
 
+
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		Field field = new Field();
+		field.print();
+		System.out.println("Enter the coordinates of the ship: ");
+		String input = scanner.nextLine();
+		char row1;
+		int col1;
+		char row2;
+		int col2;
+		try {
+			row1 = input.charAt(0);
+			col1 =  Character.getNumericValue(input.charAt(1));
+			row2 = input.charAt(3);
+			col2 = Character.getNumericValue(input.charAt(4));
+			if (row1 == row2 | col1 == col2) {
+				//System.out.println("OK!");
+			} else {
+				System.out.println("Error!");
+			}
+		} catch (Exception e){
+			System.out.println("Error!");
+		}
+		
 	}
 
 }
