2dbf64d25decd32a0ee0f742931de74a53d16922
Homaar0 <144428272+Homaar0@users.noreply.github.com>
2025-02-05 13:55:14 +0100

    Fixed condition for ship coordinates
    stage 1 completed

--- lib/src/main/java/_Battleship/Main.java
+++ lib/src/main/java/_Battleship/Main.java
@@ -20,12 +20,12 @@ public class Main {
 			col1 = input.charAt(2) == ' ' ? Character.getNumericValue(input.charAt(1)) : input.charAt(2) == '0' && input.charAt(1) == '1'? 10 : 11;			
 			row2 = input.charAt(2) == ' ' ? input.charAt(3) : input.charAt(4);
 			if (input.charAt(2) == ' ') {
-				col2 = input.charAt(5) == '\n' ? Character.getNumericValue(input.charAt(4)) : input.charAt(5) == '0' && input.charAt(4) == '1'? 10 : 11;
+				col2 = input.length() == 5 ? Character.getNumericValue(input.charAt(4)) : input.charAt(5) == '0' && input.charAt(4) == '1'? 10 : 11;
 			} else {
-				col2 = input.charAt(6) == '\n' ? Character.getNumericValue(input.charAt(5)) : input.charAt(6) == '0' && input.charAt(5) == '1'? 10 : 11;
+				col2 = input.length() == 6 ? Character.getNumericValue(input.charAt(5)) : input.charAt(6) == '0' && input.charAt(5) == '1'? 10 : 11;
 			}
 			
-			if ((row1 == row2 | col1 == col2) && (row1 <= 'J' && row2 <= 'J') && (col1 <= 10 && col2 <= 10)) {
+			if ((row1 == row2 | col1 == col2) && (row1 <= 'J' && row2 <= 'J' && row1 >= 'A' && row2 >= 'A') && (col1 <= 10 && col2 <= 10 && col1 > 0 && col2 > 0)) {
 				//System.out.println("OK!");
 				Ship ship = new Ship(row1, col1, row2, col2);
 				System.out.println("Length: " + ship.getLength());
@@ -36,7 +36,7 @@ public class Main {
 			}
 		} catch (Exception e){
 			System.out.println("Error!");
-			System.out.println("Catched exception e");
+			//System.out.println("Catched exception e");
 		}
 		
 	}
